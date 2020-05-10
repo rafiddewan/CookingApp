36ba4bf015be06e0703d01bb34d4212c400487d0
rafiddewan <rafid.dewan145@gmail.com>
2020-05-10 13:13:10 -0400

    Created shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@
 
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader, elementStrings } from './views/base';
@@ -88,7 +89,6 @@ const controlRecipe = async () => {
         try{
             // Get Recipe data and parse ingredients
             await state.recipe.getRecipe();
-            console.log(state.recipe.ingredients);
             state.recipe.parseIngredients();
             //Calculate servings and time
             state.recipe.calcTime();
@@ -114,8 +114,9 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
     }
     recipeView.updateServingsIngredients(state.recipe);
-    console.log(state.recipe);
 });
+
+window.l = new List();
 // const r = new Recipe(46956);
 // r.getRecipe();
 // console.log(r);
